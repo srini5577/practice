481afaa0a026382c6b0fbc1e788752b2da77a2a9
Spectrum <Spectrum@DESKTOP-K8GACO0>
2019-06-28 16:59:32 +0200

    """"seciond commmit"""""

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,6 +3,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) { 
 System.out.println("print me");
+System.out.println("print me agaian");
+System.out.println("print me agaian 3");
 
 	
 
